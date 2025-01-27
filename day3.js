da051fad2e9e4df9a3a020318bf9637e5dd0fa26
mustafaaadil448 <mdmustafa8210@gmail.com>
2025-01-27 13:59:19 +0530

    commited

---
+++ day3.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe:function(val2){
+            if(val!==val2) {
+                throw new Error("Not Equal");
+            } else {
+                return true;
+            }
+        },
+        notToBe:function(val2){
+            if(val===val2) {
+            throw new Error("Equal");
+            } else {
+                return true;
+            }
+            
+        }
+    }
+}
